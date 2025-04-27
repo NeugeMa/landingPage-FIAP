4e1943db82a7ecb19ed58d0ca4c786f8c41bac30
NeugeMa <mariana.n.dourado@gmail.com>
2025-04-27 20:42:39 -0300

    Atualizações: Adicionamos códigos JS para a animaçãode fade-in + rolamento de scroll

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+// Criação Animação
+window.addEventListener('load', () => {
+    // Seleciona todos os elementos com a classe 'fade-in'
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    // Adiciona a classe 'show' para ativar a animação
+    fadeInElements.forEach((element) => {
+        element.classList.add('show');
+    });
+});
+
+// ------------------------------------------------------------- //
+// Criação Scroll
+const scrollButton = document.querySelector('.scroll-down');
+
+// Adiciona o evento de clique
+scrollButton.addEventListener('click', (event) => {
+    event.preventDefault(); 
+
+    // Sessão de destino
+    const targetSection = document.querySelector('#educacao');
+
+    // Rolamento suave 
+    targetSection.scrollIntoView({
+        behavior: 'smooth', 
+        block: 'start' 
+    });
+});
+
+// ------------------------------------------------------------- //
 // Criação Modal 
 const modal = document.getElementById('modal');
 const modalImage = document.getElementById('modal-image');
